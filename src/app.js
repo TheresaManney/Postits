2cc002d5168b1ce7343724a1b5c3a7324b23a78f
Theresa Manney <theresa.m.17@gmail.com>
2017-05-30 13:23:08 -0700

    Wave 1 complete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import _ from 'underscore';
 import $ from 'jquery';
 import 'jquery-colpick';
+import Postit from './models/postit.js';
 
 
 var postitData = [{
@@ -12,8 +13,46 @@ var postitData = [{
     color: "#752310"
 }];
 
+var myPostit = new Postit( {
+  text: "What a beautiful postit!",
+  color: '#8A4FFF'
+});
+
+var getFormData = function() {
+  var formText = $('#text').val();
+  $('#text').val('');
+
+  var formColor = $('#color').val();
+  // $('#color').var('');
+
+  return {
+    text: formText,
+    color: formColor
+  };
+};
+
+var render = function(postit) {
+  var templateText = $('#postit-template').html();
+
+  var templateObject = _.template(templateText);
+
+  var compiledHTML = templateObject(postit.toJSON());
+
+  $('#postits').append(compiledHTML);
+};
+
 $(document).ready(function() {
-  console.log("Lets go!");
+  // console.log("Lets go!");
+  render(new Postit(postitData[0]));
+  render(myPostit);
+  console.log(myPostit);
+
+  $("button.success").click(function() {
+    console.log('test');
+    var formData = getFormData();
+    var newPostit = new Postit(formData);
+    render(newPostit);
+  });
 });
 
 // end
