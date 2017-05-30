d7f1a9d8717f185e000c9248f3cf06b8a66ea8e4
Theresa Manney <theresa.m.17@gmail.com>
2017-05-30 14:36:26 -0700

    added things to postit_note.js

--- src/collections/postit_notes.js
+++ src/collections/postit_notes.js
@@ -1,4 +1,11 @@
 // postit_notes.js
 
 import Backbone from 'backbone';
+
 import Postit from '../models/postit.js';
+
+var PostitList = Backbone.Collection.extend({
+  model: Postit
+});
+
+export default PostitList;
